f70537225d0a377304cbe493ced4666af186f6c2
kai815 <h.kaizuka815@gmail.com>
2020-08-17 14:58:19 +0900

    初期の黒い背景の描画

---
+++ src/main.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+  init()
+}
+//初期の黒い背景の描画
+function init(){
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext('2d');
+  const WINDOW_WIDTH = window.innerWidth;
+  const WINDOW_HEIGHT = window.innerHeight;
+  canvas.width = WINDOW_WIDTH;
+  canvas.height = WINDOW_HEIGHT;
+  console.log(WINDOW_WIDTH)
+  console.log(WINDOW_HEIGHT)
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+}
